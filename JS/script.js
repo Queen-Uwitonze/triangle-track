2726235d1d9477b7ae2fa2aa3515022f67fcfac5
Queen-Uwitonze <uwitonzeq@gmail.com>
2018-12-14 01:31:40 -0500

    i added first condition if

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,9 @@ var angle1=parseInt(prompt("enter degree of angle1"));
 var angle2=parseInt(prompt("enter degree of angle2"));
 var angle3=parseInt(prompt("enter degree of angle3"));
 
+if (angle1 === angle2 && angle2=== angle3 && angle1===angle3){
+    console.log(alert("the triangle is Equirateral"));
+    }
         
         
         
