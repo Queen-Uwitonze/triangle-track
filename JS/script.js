22f9d4692d88122a64a010a004205e3272ae5935
Queen-Uwitonze <uwitonzeq@gmail.com>
2018-12-14 06:29:30 -0500

    i am styling it in css

--- JS/script.js
+++ JS/script.js
@@ -5,7 +5,7 @@ function triangleAngle(){
     var angle3=parseInt(document.getElementById('angle3').value);
 
     if (angle1 === angle2 && angle2=== angle3 && angle1===angle3){
-        console.log(alert("the triangle is Equirateral"));
+        console.log(alert(<a href="id=one"></a>));
         }  else if (angle1===angle2 && angle1<angle3 || angle2<angle3){
             console.log(alert("the triangle is Isosceles"));
         } else if (angle1!==angle2 && angle2!==angle3 && angle3!==angle1){
