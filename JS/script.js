533ba984fd2146344608b89c3fb2dffc12fcb6a4
Queen-Uwitonze <uwitonzeq@gmail.com>
2018-12-14 01:26:28 -0500

    i modified js/script.js

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,8 @@
+var triangleTracker=180;
+
+var angle1=parseInt(prompt("enter degree of angle1"));
+var angle2=parseInt(prompt("enter degree of angle2"));
+var angle3=parseInt(prompt("enter degree of angle3"));
 
         
         
