ad1f422e879cf394a74a9a11497a155507b88d24
Queen-Uwitonze <uwitonzeq@gmail.com>
2018-12-14 01:33:50 -0500

    i added a second condition else if

--- JS/script.js
+++ JS/script.js
@@ -6,7 +6,9 @@ var angle3=parseInt(prompt("enter degree of angle3"));
 
 if (angle1 === angle2 && angle2=== angle3 && angle1===angle3){
     console.log(alert("the triangle is Equirateral"));
-    }
+    }  else if (angle1===angle2 && angle1<angle3 || angle2<angle3){
+        console.log(alert("the triangle is Isosceles"));
+    } 
         
         
         
