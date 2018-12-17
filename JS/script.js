375dee7923203ab0660791c920e4fe2a23ba8c8c
Queen-Uwitonze <uwitonzeq@gmail.com>
2018-12-17 04:25:28 -0500

    just modified in html,css and js and i have  created a  readme file

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,17 @@
-
-function triangleAngle(){
-    var angle1=parseInt(document.getElementById('angle1').value);
-    var angle2=parseInt(document.getElementById('angle2').value);
-    var angle3=parseInt(document.getElementById('angle3').value);
-
-    if (angle1 === angle2 && angle2=== angle3 && angle1===angle3){
-        console.log(alert(<a href="id=one"></a>));
-        }  else if (angle1===angle2 && angle1<angle3 || angle2<angle3){
-            console.log(alert("the triangle is Isosceles"));
-        } else if (angle1!==angle2 && angle2!==angle3 && angle3!==angle1){
-            console.log(alert("the triangle is Scalene"));
-        } else {
-    console.log(alert("the triangle does not existed"));}
-        }
+var total =180;
+function triangleAngle() {
+  var angle1 = parseInt(document.getElementById("angle1").value);
+  var angle2 = parseInt(document.getElementById("angle2").value);
+  var angle3 = parseInt(document.getElementById("angle3").value);
+  
+  
+  if(angle1 === angle2 && angle2 === angle3 && angle1 === angle3) {
+    console.log(alert("the triangle is Equirateral"));
+  } else if (angle1 === angle2 && angle1 + angle2 === angle3 ) {
+    console.log(alert("the triangle is Isosceles"));
+  } else if (angle1 !== angle2 && angle2 !== angle3 && angle1 !== angle3 && angle3!==0) {
+    console.log(alert("the triangle is Scalene"));
+  } else {
+    console.log(alert("the triangle does not existed"));
+  }
+}
