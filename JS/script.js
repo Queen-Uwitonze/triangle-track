b0206326c874232c2d0dc2b9e107d12f8fa8218c
Queen-Uwitonze <uwitonzeq@gmail.com>
2018-12-17 06:17:56 -0500

    added arrays and looped them

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,13 @@
-var total =180;
+
 function triangleAngle() {
   var angle1 = parseInt(document.getElementById("angle1").value);
   var angle2 = parseInt(document.getElementById("angle2").value);
   var angle3 = parseInt(document.getElementById("angle3").value);
   
+  var angles=["angle1","angle2","angle3"]
+  angles.forEach(function(angle){
+   console.log("angle's number is " + angle)
+  });
   
   if (angle1+angle2>angle3 && angle1+angle3>angle2 && angle2+angle3>angle1){
     if (angle1 === angle2 && angle2 === angle3 && angle1 === angle3) {
